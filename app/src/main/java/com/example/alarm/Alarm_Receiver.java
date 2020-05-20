9223807a69c631c3a7e1563ea206dfdf8d46318d
A-Kvisgaard <35926878+A-Kvisgaard@users.noreply.github.com>
2020-05-20 20:33:09 +0200

    Notification when alarm is recived

---
+++ app/src/main/java/com/example/alarm/Alarm_Receiver.java
@@ -0,0 +1,20 @@
+package com.example.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class Alarm_Receiver  extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e("Recived","TRUE");
+        NotificationPusher pusher = new NotificationPusher(context);
+        NotificationCompat.Builder nb = pusher.getChannelNotification("Alarm!","Wake up you lazy bugger");
+        pusher.getManager().notify(1, nb.build());
+    }
+}
--- app/src/main/java/com/example/alarm/MainActivity.java
+++ app/src/main/java/com/example/alarm/MainActivity.java
@@ -2,38 +2,74 @@ package com.example.alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    PendingIntent pendingIntent;
     TimePicker picker;
     TextView alarmStatusText;
-    Button alarmOn, alarmOff;
+    AlarmManager alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmStatusText = findViewById(R.id.AlarmStatusText);
 
         picker = findViewById(R.id.AlertTimePicker);
         picker.setIs24HourView(true);
 
-        alarmOn = findViewById(R.id.AlarmOn);
-        alarmOff = findViewById(R.id.AlarmOff);
-
-
     }
     public void AlarmOn(View v){
-        alarmStatusText = findViewById(R.id.AlarmStatusText);
-        alarmStatusText.setText(R.string.alarm_on_text);
+        setStatusText(R.string.alarm_on_text);
+        Intent intent = new Intent(this, Alarm_Receiver.class);
+        intent.putExtra("extra", true);
+        pendingIntent = PendingIntent.getBroadcast(this,1,intent,0);
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, getPickerTime(), pendingIntent);
     }
 
     public void AlarmOff(View v){
-        alarmStatusText = findViewById(R.id.AlarmStatusText);
-        alarmStatusText.setText(R.string.alarm_off_text);
+        if (pendingIntent != null) {
+            setStatusText(R.string.alarm_off_text);
+            toast(getString(R.string.alarm_off_text));
+            alarmManager.cancel(pendingIntent);
+            pendingIntent = null;
+        }
     }
+
+    private void setStatusText(int output){
+        alarmStatusText.setText(output);
+    }
+
+    private long getPickerTime(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, picker.getHour());
+        calendar.set(Calendar.MINUTE, picker.getMinute());
+        calendar.set(Calendar.SECOND, 0);
+
+        if (calendar.before(Calendar.getInstance())){
+            calendar.add(Calendar.DATE, 1);
+        }
+        String date = calendar.get(Calendar.MONTH) +"/" + calendar.get(Calendar.DATE) + " " + DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.getTime());
+        toast(getString(R.string.alarm_on_text) + ": " + date);
+        return calendar.getTimeInMillis();
+    }
+    private void toast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
 }
