7e5d909cf45fe1343c62582a457e811bccfd85d7
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2020-05-21 22:56:41 +0200

    Preliminary stuff

---
+++ app/src/main/java/com/example/alarm/DataGatherer.java
@@ -0,0 +1,61 @@
+package com.example.alarm;
+
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DataGatherer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+
+    public void APICall(View view/*double lat, double lon*/){
+        //JSONObject jobject = new JSONObject();
+        //String APIKey = ""; //Ved ikke hvad APIKey skull være endnu. Har ikke læst nok op på det.
+        //String url = "https://api.openweathermap.org/data/2.5/onecall?lat={" + lat + "}&lon={" + lon + "}&exclude={part}&appid={" + APIKey + "}";
+        final TextView textView = findViewById(R.id.textView3);
+        textView.setText("Pasta");
+        String url = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02";
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
